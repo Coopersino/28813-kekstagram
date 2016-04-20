c3ce9cdac435b0c390668a2e10f99ae9f858ce93
Coopersino <vs-mailbox@ya.ru>
2016-04-20 18:36:17 +0300

    Поправил кодстайл

--- src/pictures.js
+++ src/pictures.js
@@ -2,8 +2,8 @@
 var filtersBlock = document.querySelector('.filters');
 var picturesContainer = document.querySelector('.pictures');
 var pictureTemplate = document.querySelector('#picture-template');
-var cloneElement = undefined;
-var imageLoadTimeout = undefined;
+var cloneElement;
+var imageLoadTimeout;
 
 var IMAGE_TIMEOUT = 10000; //устанавливаем таймаут 10 секунд
 
@@ -15,7 +15,7 @@ if ('content' in pictureTemplate) {
   cloneElement = pictureTemplate.querySelector('.picture');
 }
 
-var getPicture = function (data, container) {
+var getPicture = function(data, container) {
   var element = cloneElement.cloneNode(true);
   element.querySelector('.picture-comments').textContent = data.comments;
   element.querySelector('.picture-likes').textContent = data.likes;
@@ -25,21 +25,21 @@ var getPicture = function (data, container) {
   pictureItem.width = 182;
   pictureItem.height = 182;
 
-  pictureItem.onload = function () {
+  pictureItem.onload = function() {
     clearTimeout(imageLoadTimeout);
   };
-  pictureItem.onerror = function () {
+  pictureItem.onerror = function() {
     element.classList.add('picture-load-failure');
   };
 
   pictureItem.src = data.url;
-  imageLoadTimeout = setTimeout(function () {
+  imageLoadTimeout = setTimeout(function() {
     pictureItem.src = '';
     element.classList.add('picture-load-failure');
   }, IMAGE_TIMEOUT);
 };
 
 var arrayPictures = window.pictures;
-arrayPictures.forEach(function (picture) {
+arrayPictures.forEach(function(picture) {
   getPicture(picture, picturesContainer);
 });
