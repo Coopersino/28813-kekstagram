31415da894c34378adeb3bd4a98be71c668be57b
Coopersino <vs-mailbox@ya.ru>
2016-05-03 22:40:07 +0300

    Поправил localStorage

--- src/filter.js
+++ src/filter.js
@@ -44,7 +44,7 @@ var getFilterFromLocalStorage = function() {
 };
 
 var getCurrentFilter = function() {
-  if(localStorage.hasOwnProperty('filter')) {
+  if (getFilterFromLocalStorage('filter')) {
     utilities.filtersBlock.querySelector('#' + getFilterFromLocalStorage()).setAttribute('checked', true);
     return getFilterFromLocalStorage();
   } else {
